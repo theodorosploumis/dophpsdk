9b47ac044669a14c27acef9355ddedf7770f18af
TheodorosPloumis <me@theodorosploumis.com>
2017-12-13 01:44:03 +0200

    Refactor classes

--- README.md
+++ README.md
@@ -29,35 +29,104 @@ For all the available functions see the classes [Container.php](https://github.c
 ```
 <?php
 
-require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/src/Docker.php";
+require_once __DIR__ . "/src/Container.php";
+
+include 'vendor/autoload.php';
 
 use dophpsdk\Docker;
 use dophpsdk\Container;
 
+$name = "drupal";
+$image = "drupal:8.4.3";
+$container_name = "drupal_4";
+
 // Create some new Container objects for image https://hub.docker.com/_/drupal
-for ($i = 1; $i <= 4; $i++) {
-    $container = new Container("drupal:8.4.3", "drupal_" . $i);
+ for ($i = 1; $i <= 5; $i++) {
+   $settings = [
+     "Name" => $name . "_" . $i,
+     "Config" => [
+       "Image" => $image
+     ]
+   ];
+   
+   $container = new Container($settings);
 
-    // Start a container
-    $container->run();
-
-    // Get the container id
-    print $container->getId();
-
-    // Inspect a container
-    var_dump($container->inspect());
-}
-
+   // Get the Container object
+   var_dump($container);
+ }
+ 
 // Create a new Docker object
 $docker = new Docker();
 
-// Get running containers
-$docker_running_containers = $docker->ps();
-var_dump($docker_running_containers);
+$info = $docker->info();
+var_dump($info);
 
-// Get docker images
-$docker_images = $docker->images();
-var_dump($docker_images);
+$version = $docker->version();
+var_dump($version);
+
+$images = $docker->images();
+var_dump($images);
+
+$ps = $docker->ps();
+var_dump($ps);
+
+$stats = $docker->ContainerStats();
+var_dump($stats);
+
+$logs = $docker->ContainerLogs($container_name);
+var_dump($logs);
+
+$docker->ContainerStop($container_name);
+$docker->ContainerStart($container_name);
+$docker->ContainerRestart($container_name);
+
+$isRunning = $docker->ContainerIsRunning($container_name);
+
+if($isRunning == TRUE) {
+  print "Yes, container " . $container_name . " is running\n";
+} else if (!is_null($isRunning) && $isRunning == FALSE) {
+  print "No, container " . $container_name . " is not running\n";
+} else {
+  print "There is no such container " . $name . "\n";
+}
+
+$create = $docker->ContainerCreate($image);
+var_dump($create);
+
+$run = $docker->ContainerRun($image);
+var_dump($run);
+
+$rm = $docker->ContainerRm("drupal_1");
+var_dump($rm);
+
+$kill = $docker->ContainerKill("drupal_1");
+var_dump($kill);
+
+$delete = $docker->ContainerDelete("drupal_2");
+var_dump($delete);
+
+$rename = $docker->ContainerRename("drupal_4", "My name");
+var_dump($rename);
+
+$exec = $docker->ContainerExec("drupal_4", "ls -l");
+var_dump($exec);
+
+$update = $docker->ContainerUpdate("drupal_4", "--restart on-failure");
+var_dump($docker->ContainerInspect("drupal_4"));
+
+$docker->ContainerPause("drupal_4");
+$unpause = $docker->ContainerUnpause("drupal_4");
+var_dump($unpause);
+
+$commit = $docker->ContainerCommit("drupal_4", "test/test");
+var_dump($docker->images("test/test"));
+
+$rmi = $docker->rmi("test/test");
+var_dump($rmi);
+
+$diff = $docker->ContainerDiff("drupal_4");
+var_dump($diff);
 
 ```
 
--- src/Docker.php
+++ src/Docker.php
@@ -7,31 +7,404 @@ namespace dophpsdk;
  * Several docker management commands and utilities.
  */
 class Docker {
-
-  public function images($options = "", $image = "") {
-    $cmd = "docker images ";
-    $cmd .= $options;
-    $cmd .= " --format ";
-    $cmd .= '\"{{.ID}}\":{\"image\":\"{{.Repository}}:{{.Tag}}\",\"created\":\"{{.CreatedSince}}\",\"size\":\"{{.Size}}\"}';
+  
+  /**
+   * @ToDo: Make this abstract to allow usage for more than one containers
+   *
+   * Start one or more stopped containers.
+   * See "docker start --help" for more details.
+   *
+   * @param string $unique
+   * @return bool
+   */
+  public function ContainerStart($unique) {
+    $cmd = "docker start " . $unique;
+    exec($cmd, $result);
+    return $result == $unique;
+  }
+  
+  /**
+   * @ToDo: Make this abstract to allow usage for more than one containers.
+   *
+   * Restart one or more containers.
+   * See "docker restart --help" for more details.
+   *
+   * @param string $unique
+   * @return bool
+   */
+  public function ContainerRestart($unique) {
+    $cmd = "docker restart " . $unique;
+    exec($cmd, $result);
+    return $result == $unique;
+  }
+  
+  /**
+   * @ToDo: Make this abstract to allow usage for more than one containers.
+   *
+   * Stop one or more containers.
+   * See "docker stop --help" for more details.
+   *
+   * @param string $unique
+   * @return bool
+   */
+  public function ContainerStop($unique) {
+    $cmd = "docker stop " . $unique;
+    exec($cmd, $result);
+    return $result == $unique;
+  }
+  
+  /**
+   * Check if container is running.
+   * If running returns "true" else "false".
+   * If there is no such container returns "null".
+   *
+   * @param string $unique
+   * @return boolean|null
+   */
+  public function ContainerIsRunning($unique) {
+    $cmd = "docker inspect --format='{{.State.Running}}' ";
+    $cmd .= $unique;
+    $cmd .= " 2>&1";
+  
+    exec($cmd, $result);
+    
+    if (isset($result[1])) {
+      return NULL;
+    }
+    
+    if ($result[0] == "false") {
+      return FALSE;
+    } else {
+      return TRUE;
+    }
+  }
+  
+  /**
+   * Create a new container.
+   * See "docker create --help" for more details.
+   *
+   * Returns the new container id or FALSE.
+   *
+   * @param string $options
+   * @param string $name
+   * @param string $image
+   *
+   * @return string|bool
+   */
+  public function ContainerCreate($image, $name = "", $options = "") {
+    $cmd = "docker create ";
+    if ($name) {
+      $cmd .= " --name " . $name;
+    }
+    $cmd .= " " . $options;
     $cmd .= " " . $image;
+    $cmd .= " 2>&1";
+    
+    exec($cmd, $result);
+    
+    if (isset($result[0]) && !isset($result[1])) {
+      // If success returns the container ID
+      return $result;
+    } else {
+      return FALSE;
+    }
+  }
+  
+  /**
+   * Run a command in a new container.
+   * See "docker run --help" for more details.
+   * Notice that there is no need to add the "/bin/sh -c" before the actual command.
+   *
+   * @param string $image
+   * @param string $name
+   * @param string $command
+   * @param string $options
+   *
+   * @return string|bool
+   */
+  public function ContainerRun($image, $name = "", $command = "", $options = "") {
+    $cmd = "docker run -d ";
+    if ($name) {
+      $cmd .= "--name " . $name;
+    }
+    $cmd .= " " . $options;
+    $cmd .= " " . $image;
+    if ($command) {
+      $cmd .= " /bin/sh -c " . $command;
+    }
+    
+    exec($cmd, $result);
+  
+    if (isset($result[0]) && !isset($result[1])) {
+      // If success returns the container ID
+      return $result;
+    } else {
+      return FALSE;
+    }
+  }
+  
+  /**
+   * Remove a container.
+   * See "docker rm --help" for more details.
+   * Notice that if the container is running it will not be removed except if using "-f" as options.
+   *
+   * @param string $unique
+   * @param string $options
+   *
+   * @return bool
+   */
+  public function ContainerRm($unique, $options = "") {
+    $cmd = "docker rm ";
+    $cmd .= $options;
+    $cmd .= " " . $unique;
+    $cmd .= " 2>&1";
+  
+    exec($cmd, $result);
+  
+    if ($result[0] == $unique) {
+      // If success returns TRUE
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+  
+  /**
+   * Kill a container.
+   * See "docker kill --help" for more details.
+   *
+   * @param string $unique
+   * @param string $options
+   *
+   * @return bool
+   */
+  public function ContainerKill($unique, $options = "") {
+    $cmd = "docker kill ";
+    $cmd .= $options;
+    $cmd .= " " . $unique;
+    $cmd .= " 2>&1";
+    
+    exec($cmd, $result);
+    
+    if ($result[0] == $unique) {
+      // If success returns TRUE
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+  
+  /**
+   * Delete a container for ever. Use with caution.
+   *
+   * @param $unique
+   *
+   * @return bool
+   */
+  public function ContainerDelete($unique) {
+    return $this->ContainerRm($unique, "-f");
+  }
+  
+  /**
+   * Rename a container (change name).
+   * See "docker rename --help" for more details.
+   *
+   * @param string $name
+   * @param string $unique
+   *
+   * @return bool
+   */
+  public function ContainerRename($unique, $name) {
+    $cmd = "docker rename ";
+    $cmd .= $unique;
+    $cmd .= " " . $this::sanitizeContainerName($name);
+    
+    exec($cmd, $result);
+    
+    if (!isset($result[0])) {
+      // If success returns TRUE
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+  
+  /**
+   * Inspect a container using Id or Name.
+   * See "docker inspect --help" for more details.
+   *
+   * Returns an array from the json data.
+   * We can use var_damp etc to get the array keys/values.
+   *
+   * @param string $options
+   * @param string $unique
+   *
+   * @return mixed
+   */
+  public function ContainerInspect($unique, $options = "") {
+    $cmd = "docker inspect ";
+    $cmd .= $options;
+    $cmd .= "--format '{{json .}}' ";
+    $cmd .= " " . $unique;
+    
+    exec($cmd, $result);
+    
+    return $this::JsonOutputToArray($result, "Id");
+  }
+  
+  /**
+   * Fetch the logs of a container.
+   * See "docker logs --help" for more details.
+   *
+   * @param string $options
+   * @param string $unique
+   *
+   * @return array
+   */
+  public function ContainerLogs($unique, $options = "") {
+    $cmd = "docker logs ";
+    $cmd .= $options . " ";
+    $cmd .= $unique;
+    $cmd .= " 2>&1";
 
     exec($cmd, $result);
-
-    // Create a temporary json file
-    $file = "/tmp/docker_images_" . mt_rand(0,999) . ".json";
-    $result = "{\n" . implode(',', $result) . "\n}";
-    file_put_contents($file, $result);
-
-    // Get array from json
-    $array = json_decode(file_get_contents($file), TRUE);
-
-    // Delete file
-    unlink($file);
-
-    // Return the final php array
-    return $array;
+    
+    return $result;
   }
-
+  
+  /**
+   * Get container stats in array.
+   * See "docker stats --help" for more details.
+   *
+   * @param string $unique
+   * @return mixed
+   */
+  public function ContainerStats($unique = "") {
+    $cmd = "docker stats ";
+    $cmd .= " --no-stream --no-trunc --format '{{json .}}' ";
+    $cmd .= $unique;
+    
+    exec($cmd, $result);
+    
+    return $this->JsonOutputToArray($result, "ID");
+  }
+  
+  /**
+   * Run a command in a running container.
+   * See "docker exec --help" for more details.
+   *
+   * @param string $unique
+   * @param string $command
+   *
+   * @return array|bool
+   */
+  public function ContainerExec($unique, $command) {
+    $cmd = "docker exec -ti ";
+    $cmd .= " ". $unique;
+    $cmd .= " ". $command;
+    $cmd .= " 2>&1";
+    
+    exec($cmd, $result);
+    
+    if ($result[0] == "Error: No such container: " . $unique) {
+      return FALSE;
+    } else {
+      return $result;
+    }
+  }
+  
+  /**
+   * Update configuration of one or more containers.
+   * See "docker update --help" for more details.
+   *
+   * @param string $unique
+   * @param string $options
+   *
+   * @return bool
+   */
+  public function ContainerUpdate($unique, $options) {
+    $cmd = "docker update ";
+    $cmd .= $options;
+    $cmd .= " " . $unique;
+    $cmd .= " 2>&1";
+    
+    exec($cmd, $result);
+    
+    if ($result[0] == $unique) {
+      return TRUE;
+    } else {
+      return $result;
+    }
+  }
+  
+  /**
+   * Pause all processes within a container.
+   * See "docker pause --help" for more details.
+   *
+   * @param string $unique
+   *
+   * @return array|bool
+   */
+  public function ContainerPause($unique) {
+    $cmd = "docker pause " . $unique;
+    $cmd .= " 2>&1";
+    
+    exec($cmd, $result);
+  
+    if (isset($result[0]) && $result[0] == $unique) {
+      return TRUE;
+    } else {
+      return $result;
+    }
+  }
+  
+  /**
+   * Unpause all processes within a container.
+   * See "docker unpause --help" for more details.
+   *
+   * @param string $unique
+   *
+   * @return array|bool
+   */
+  public function ContainerUnpause($unique) {
+    $cmd = "docker unpause " . $unique;
+    $cmd .= " 2>&1";
+    
+    exec($cmd, $result);
+  
+    if (isset($result[0]) && $result[0] == $unique) {
+      return TRUE;
+    } else {
+      return $result;
+    }
+  }
+  
+  /**
+   * Commit a container to create a new image.
+   * See "docker commit --help" for more details.
+   *
+   * @param string $image | The new docker image in the format [image]:[tag]
+   * @param string $unique
+   * @param string $options
+   *
+   * @return array|bool
+   */
+  public function ContainerCommit($unique, $image, $options = "") {
+    $cmd = "docker commit ";
+    $cmd .= $options;
+    $cmd .= " " . $unique;
+    $cmd .= " " . $image;
+    $cmd .= " 2>&1";
+  
+    exec($cmd, $result);
+  
+    if (isset($result[0]) && $result[0] == $unique) {
+      return TRUE;
+    } else {
+      return $result;
+    }
+  }
+  
   /**
    * List containers.
    * See "docker ps --help" for more details.
@@ -42,91 +415,148 @@ class Docker {
   public function ps($options = "-a") {
     $cmd = "docker ps ";
     $cmd .= $options;
-    $cmd .= " --format ";
-    $cmd .= '\'"{{.ID}}":{"id":"{{.ID}}","image":"{{.Image}}","created":"{{.CreatedAt}}","ports":"{{.Ports}}", "status":"{{.Status}}","names":"{{.Names}}"}\'';
-
+    $cmd .= " --format '{{json .}}'";
+    
     exec($cmd, $result);
-
-    // Create a temporary json file
-    $file = "/tmp/docker_ps" . mt_rand(0, 999) . ".json";
-    $result = "{\n" . implode(',', $result) . "\n}";
-    file_put_contents($file, $result);
-
-    // Get array from json
-    $array = json_decode(file_get_contents($file), TRUE);
-
-    // Delete file
-    unlink($file);
-
-    // Return the final php array
-    return $array;
+  
+    return $this->JsonOutputToArray($result, "ID");
   }
-
+  
   /**
-   * Get container stats in array.
-   * See "docker stats --help" for more details.
+   * List Images.
+   * See "docker images --help" for more details.
    *
-   * @param string $container_id
+   * @param string $options
+   * @param string $image
+   * @return array|bool
+   */
+  public function images($options = "", $image = "") {
+    $cmd = "docker images ";
+    $cmd .= $options;
+    $cmd .= " --no-trunc --digests --format '{{json .}}' ";
+    $cmd .= $image;
+    
+    exec($cmd, $result);
+    
+    return $this::JsonOutputToArray($result, "ID");
+  }
+  
+  /**
+   * Remove one or more images.
+   * See "docker rmi --help" for more details.
+   *
+   * @param string $image
+   * @param string $options
+   *
+   * @return array|bool
+   */
+  public function rmi($image, $options = "") {
+    $cmd = "docker rmi ";
+    $cmd .= $options;
+    $cmd .= " " . $image;
+    $cmd .= " 2>&1";
+  
+    exec($cmd, $result);
+  
+    if (strpos($result[0], 'Error') === 0) {
+      return $result;
+    } else {
+      return TRUE;
+    }
+  }
+  
+  /**
+   * Inspect changes to files or directories on a container's filesystem.
+   * See "docker diff --help" for more details.
+   *
+   * @param $unique
+   *
+   * @return array|bool
+   */
+  public function ContainerDiff($unique) {
+    $cmd = "docker diff ";
+    $cmd .= $unique;
+    $cmd .= " 2>&1";
+    
+    exec($cmd, $result);
+  
+    return $result;
+  }
+  
+  /**
+   * Docker info
+   *
    * @return array
    */
-  public function stats($container_id = "") {
-    $cmd = 'docker stats --no-stream ';
-    $cmd .= '--format ';
-    $cmd .= '\'"{{.Container}}": {"ID": "{{.Container}}","memory":{"raw":"{{.MemUsage}}","percent":"{{.MemPerc}}"},"cpu":"{{.CPUPerc}}"}\'';
-    $cmd .= " " . $container_id;
-
+  public function info() {
+    $cmd = "docker info --format '{{json .}}' ";
+    
     exec($cmd, $result);
-
-    // Create a temporary json file
-    $file = "/tmp/docker_stats " . mt_rand(0, 999) . ".json";
-    $result = "{\n" . implode(',', $result) . "\n}";
-    file_put_contents($file, $result);
-
-    // Get array from json
-    $array = json_decode(file_get_contents($file), TRUE);
-
-    // Delete file
-    unlink($file);
-
-    // Return the final php array
-    return $array;
+    
+    return json_decode($result[0], TRUE);
   }
-
+  
   /**
-   * Get a container by ID or Name.
+   * Docker version
    *
-   * @param string $unique
-   * @return \dophpsdk\Container|bool
+   * @return array
    */
-  public function getContainer($unique) {
-    $cmd = "docker inspect --format '{{.ID}}' " . $unique;
-
+  public function version() {
+    $cmd = "docker version --format '{{json .}}' ";
+    
     exec($cmd, $result);
-
-    if ($result[0] == "") {
-      print "There is no such container.";
+    
+    return json_decode($result[0], TRUE);
+  }
+  
+  /**
+   * Generate an array from a json output
+   *
+   * @param array $result
+   * @param string $id
+   *
+   * @return array|bool
+   */
+  static function JsonOutputToArray($result, $id) {
+    if ($result) {
+      $array = [];
+      foreach ($result as $key => $value) {
+        // Decode json values
+        $value = json_decode($value, TRUE);
+        // Create the new key
+        if ($value[$id]) {
+          $k = $value[$id];
+          // Add new data to array
+          $array[$k] = $value;
+        }
+      }
+      if (!empty($array)) {
+        return $array;
+      } else {
+        return FALSE;
+      }
+    } else {
       return FALSE;
     }
-    else {
-      $container = new Container("any", "any");
-      $container->setId($result[0]);
-
-      $inspect = $container->inspect();
-      $name = $inspect["Name"];
-      $name = str_replace("/", "", $name);
-
-      $image = $inspect["Config"]["Image"];
-      $command = $inspect["Config"]["Cmd"];
-      $exposedports = $inspect["Config"]["ExposedPorts"];
-
-      $container->setName($name);
-      $container->setImage($image);
-      $container->setCommand($command);
-      $container->setPorts($exposedports);
-
-      return $container;
-    }
-
+  }
+  
+  /**
+   * Sanitize strings to create the container names (helper function).
+   *
+   * @param $string
+   * @return string
+   */
+  static function sanitizeContainerName($string) {
+    //$string = strtolower($string);
+    $regex = "/[^a-zA-Z0-9\-\_\.]/";
+    
+    $string = preg_replace($regex, '', $string);
+    $string = trim(preg_replace('/[\s\t\n\r\s]+/', ' ', $string));
+    //$string = preg_replace('/__/', '_', $string);
+    
+    $string = substr($string, 0, 40);
+    
+    return $string;
   }
 
 }
